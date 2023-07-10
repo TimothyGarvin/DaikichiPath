fd40c7f56bcc7d4e1866d884fd1eec4ac3c64ba4
Timothy Garvin <timothygarvin4@gmail.com>
2023-07-10 16:03:30 -0700

    Daikichi Path Variable assignment for CD

--- src/main/java/com/garvintimothy/daikichipath/DaikichiPathApplication.java
+++ src/main/java/com/garvintimothy/daikichipath/DaikichiPathApplication.java
@@ -3,7 +3,7 @@ package com.garvintimothy.daikichipath;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class DaikichiPathApplication {
 
 	public static void main(String[] args) {
