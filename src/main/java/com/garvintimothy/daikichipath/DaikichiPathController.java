3e0a15c8b82111795b1698c520362657661ab005
Timothy Garvin <timothygarvin4@gmail.com>
2023-07-10 15:45:16 -0700

    "Daicki Path Variables" assignment for CD

---
+++ src/main/java/com/garvintimothy/daikichipath/DaikichiPathController.java
@@ -0,0 +1,24 @@
+package com.garvintimothy.daikichipath;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiPathController {
+	@RequestMapping("/location/{locationName}")
+	public String showTown(@PathVariable("locationName")String locationName) {
+		return "Congrats! You're going to " + locationName + "!";
+	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String showLotto(@PathVariable("number")int number) {
+		if(number % 2 == 0) {
+			return "You will take a journey in the near future! Be weary of adversaries!";
+		}
+		else {
+			return "Take a break! Relax with family and friends.";
+		}
+	}
+}
